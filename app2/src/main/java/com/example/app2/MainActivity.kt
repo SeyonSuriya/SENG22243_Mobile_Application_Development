5c67c33e47b998e00d6465e3ef566dfed841f96c
Seyon <suriyaseyon6@gmail.com>
2023-02-07 20:00:28 +0530

    -simple addition calculator created -new package (services) and kotlin file (Calculate) created to store logics of addition calculator

--- app2/src/main/java/com/example/app2/MainActivity.kt
+++ app2/src/main/java/com/example/app2/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.app2.services.Calculate
 import com.example.app2.ui.theme.Seng22243Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,15 +31,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TwoGreeting(){
+    var number1 by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue("", TextRange(0, 7)))
+    }
+
+    var number2 by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue("", TextRange(0, 7)))
+    }
+
+    var total by remember {
+        mutableStateOf(0)
+    }
+
+    var cal = Calculate()
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Column() {
-            Greeting(name = "Seyon")
-            Greeting(name = "Master")
-            Button(onClick = { Log.d("TwoGreeting","You Clicked!") }) {
-                Text(text = "Click Me")
+        Column {
+            OutlinedTextField(
+                value = number1,
+                onValueChange = {number1 = it},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                label = { Text("Number 1")}
+            )
+            OutlinedTextField(
+                value = number2,
+                onValueChange = {number2 = it},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                label = { Text("Number 2")}
+            )
+
+            Text("Total is $total")
+            Button(onClick = {
+                Log.d("TwoGreeting",number1.text)
+                Log.d("TwoGreeting",number2.text)
+                total = cal.add(number1.text.toInt(), number2.text.toInt())
+                Log.d("TwoGreeting","You Clicked!")
+            }) {
+                Text(text = "Add")
             }
         }
 
@@ -46,6 +80,16 @@ fun TwoGreeting(){
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name! Welcome")
+}
+
+@Composable
+//try to make this function returns integer value
+fun OutlinedNumberTextField(label: String, _text: TextFieldValue) {
+//    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+//        mutableStateOf(TextFieldValue("", TextRange(0, 7))) }
+    var text = _text
+
+
 }
 
 @Preview(showBackground = true)
---
+++ app2/src/main/java/com/example/app2/services/Calculate.kt
@@ -0,0 +1,8 @@
+package com.example.app2.services
+
+class Calculate {
+
+    fun add(number1: Int, number2: Int): Int{
+        return number1.plus(number2)
+    }
+}
